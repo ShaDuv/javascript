07f06c10bf585b856360067730c3b9cba48a4ab0
Daniel Hongel and Shawna Duvall <shawna@>
2019-06-04 16:09:04 -0700

    adds jquery for adding and removing the cat

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
 $(function() {
-  $("p").click(function(){
-    $(".kitty-showing").toggle();
-    $(".kitty-hide").toggle();
+  // $("").click(function(){
+  //   $("#cat1").toggle();
+  //   $("#cat-1").toggle();
+  // });
+  // $("#cat1").click(function(){
+  //   $("#cat1").toggle("#cat2");
+  //   // $("#cat1").toggle();
+  // });
+
+  $("#cat1").click(function() {
+    $("#cat2").removeClass();
+    $("#cat2").addClass("kitty-show");
+    $("#cat1").removeClass();
+    $("#cat1").addClass("kitty-hide");
   });
+
+
+  
 });
