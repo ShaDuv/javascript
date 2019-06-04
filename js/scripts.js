e2a69fc151779a87c88acbc78e3242e58b17fde9
Daniel Hongel and Shawna Duvall <daniel@>
2019-06-04 16:34:07 -0700

    adds 8 more cats

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,4 @@
 $(function() {
-  // $("").click(function(){
-  //   $("#cat1").toggle();
-  //   $("#cat-1").toggle();
-  // });
-  // $("#cat1").click(function(){
-  //   $("#cat1").toggle("#cat2");
-  //   // $("#cat1").toggle();
-  // });
 
   $("#cat1").click(function() {
     $("#cat2").removeClass();
@@ -15,6 +7,59 @@ $(function() {
     $("#cat1").addClass("kitty-hide");
   });
 
+  $("#cat2").click(function() {
+    $("#cat3").removeClass();
+    $("#cat3").addClass("kitty-show");
+    $("#cat2").removeClass();
+    $("#cat2").addClass("kitty-hide");
+  });
 
-  
+  $("#cat3").click(function() {
+    $("#cat4").removeClass();
+    $("#cat4").addClass("kitty-show");
+    $("#cat3").removeClass();
+    $("#cat3").addClass("kitty-hide");
+  });
+
+  $("#cat4").click(function() {
+    $("#cat5").removeClass();
+    $("#cat5").addClass("kitty-show");
+    $("#cat4").removeClass();
+    $("#cat4").addClass("kitty-hide");
+  });
+
+  $("#cat5").click(function() {
+    $("#cat6").removeClass();
+    $("#cat6").addClass("kitty-show");
+    $("#cat5").removeClass();
+    $("#cat5").addClass("kitty-hide");
+  });
+
+  $("#cat6").click(function() {
+    $("#cat7").removeClass();
+    $("#cat7").addClass("kitty-show");
+    $("#cat6").removeClass();
+    $("#cat6").addClass("kitty-hide");
+  });
+
+  $("#cat7").click(function() {
+    $("#cat8").removeClass();
+    $("#cat8").addClass("kitty-show");
+    $("#cat7").removeClass();
+    $("#cat7").addClass("kitty-hide");
+  });
+
+  $("#cat8").click(function() {
+    $("#cat9").removeClass();
+    $("#cat9").addClass("kitty-show");
+    $("#cat8").removeClass();
+    $("#cat8").addClass("kitty-hide");
+  });
+
+  $("#cat9").click(function() {
+    $("#cat1").removeClass();
+    $("#cat1").addClass("kitty-show");
+    $("#cat9").removeClass();
+    $("#cat9").addClass("kitty-hide");
+  });
 });
